c775c57eea49763243506cd41b57e3ea4f575820
huangcongyu2006 <huangcongyu2006@e141641c-d880-a6e4-911d-b54fb469a67a>
2011-06-14 14:54:30 +0000

    可以启动dbus接受方法调用。具体方法没有实现。
    使用MPRISv1.0版本。
    glib使用dbus必须pkg-config --libs --cflags gio-2.0
    
    
    git-svn-id: https://deadbeef-mpris-plugin.googlecode.com/svn/trunk@4 e141641c-d880-a6e4-911d-b54fb469a67a

--- mpris.c
+++ mpris.c
@@ -16,49 +16,30 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
-#include <dbus/dbus.h>
 #include "../../deadbeef.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "../../gettext.h"
 #include "../artwork/artwork.h"
+#include "mpris_server.h"
 
 DB_functions_t *deadbeef;
 DB_misc_t plugin;
 DB_artwork_plugin_t *artwork_plugin;
-
-#define MPRIS__DEBUG 1
-
-#ifndef MPRIS__DEBUG
-	#define debug(...)
-#else
-	#define debug(...)  do_debug(__VA_ARGS__)
-#endif
-
-#include <stdarg.h>
-/*
- * Debug
- */
-static void do_debug(const char *fmt, ...)
-{
-	va_list arg_ptr;
-	va_start(arg_ptr, fmt);
-	printf("\e[32m\e[1mMPRIS Debug Info: \e[0m\e[34m");
-	vprintf(fmt, arg_ptr);
-	printf("\e[0m\n");
-	va_end(arg_ptr);
-}
+DB_mpris_server *srv = NULL;
 
 int mpris_start (void) 
 {
 	debug("MPRIS Started....");
+	DB_mpris_server_start(&srv);
     return 0;
 }
 
 int mpris_stop (void) 
 {	
 	debug("MPRIS Stoped....");
+	DB_mpris_server_stop(srv);
     return 0;
 }
 
@@ -87,58 +68,6 @@ static int mpris_message (uint32_t id, uintptr_t ctx, uint32_t p1, uint32_t p2)
 
 	switch(id)
 	{
-	case DB_EV_NEXT:
-		debug("Next.");
-		break;
-	case DB_EV_PREV:
-		debug("Prev.");
-		break;
-	case DB_EV_PLAY_NUM:
-		debug("Play Num : %d.", p1);
-		break;
-	case DB_EV_STOP:
-		debug("Stop.");
-		break;
-	case DB_EV_PAUSE:
-		debug("Pause.");
-		break;
-	case DB_EV_TERMINATE:
-		debug("Terminate.");
-		break;
-	case DB_EV_CONFIGCHANGED:
-		debug("Config changed.");
-		break;
-	case DB_EV_TOGGLE_PAUSE:
-		debug("Toggle pause.");
-		break;
-	case DB_EV_ACTIVATED:
-		debug("Actived.");
-		break;
-	case DB_EV_PAUSED:
-		debug("Paused.");
-		break;
-	case DB_EV_SEEK:
-		debug("Seek to %d.", p1);
-		break;
-	case DB_EV_SONGCHANGED:
-		debug("Song changed.");
-		break;
-	case DB_EV_SONGSTARTED:
-		if(track != NULL){
-        	album = deadbeef->pl_find_meta(track, "album");
-        	artist = deadbeef->pl_find_meta(track, "artist");
-        	title = deadbeef->pl_find_meta(track, "title");
-			debug("Song started. %s %s %s", title, album, artist);
-		}else{
-			debug("Song started.");
-		}
-		break;
-	case DB_EV_SONGFINISHED:
-		debug("Song finished.");
-		break;
-	case DB_EV_TRACKINFOCHANGED:
-		debug("Track info changed.");
-		break;
 	case DB_EV_SEEKED:
 		debug("Seeked.");
 		break;
---
+++ mpris_server.c
@@ -0,0 +1,138 @@
+#include "mpris_server.h"
+#include "introspection_xml.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <gio/gio.h>
+
+/*
+ * Debug
+ */
+void do_debug(const char *fmt, ...)
+{
+	va_list arg_ptr;
+	va_start(arg_ptr, fmt);
+	printf("\e[32m\e[1mMPRIS Debug Info: \e[0m\e[34m");
+	vprintf(fmt, arg_ptr);
+	printf("\e[0m\n");
+	va_end(arg_ptr);
+}
+
+struct _DB_mpris_server
+{
+	GDBusNodeInfo *introspection_data;
+	guint owner_id;
+  	guint registration_id;
+};
+
+/*
+ * Information. 
+ */
+static DB_mpris_server *server = NULL;
+
+static void handle_method_call(GDBusConnection *connection,
+                    			const gchar *sender,
+                    			const gchar *object_path,
+                    			const gchar *interface_name,
+                    			const gchar *method_name,
+                    			GVariant *parameters,
+                    			GDBusMethodInvocation *invocation,
+                    			gpointer user_data)
+{
+	debug("Method call: %s %s %s", object_path, interface_name, method_name);
+}
+
+static GVariant *handle_get_property(GDBusConnection *connection,
+                     				const gchar *sender,
+                     				const gchar *object_path,
+                     				const gchar *interface_name,
+                     				const gchar *property_name,
+                     				GError **error,
+                     				gpointer user_data)
+{
+	return NULL;
+}
+
+static gboolean handle_set_property(GDBusConnection *connection,
+                     				const gchar *sender,
+                     				const gchar *object_path,
+                     				const gchar *interface_name,
+                     				const gchar*property_name,
+                     				GVariant *value,
+                     				GError **error,
+                     				gpointer user_data)
+{
+
+}
+
+/* vtable */
+static const GDBusInterfaceVTable interface_vtable =
+{
+	handle_method_call,
+  	handle_get_property,
+  	handle_set_property
+};
+static void on_bus_acquired (GDBusConnection *connection,
+                	 			const gchar *name,
+                 				gpointer user_data)
+{
+  	server -> registration_id = g_dbus_connection_register_object(
+		  					connection
+                            , MPRIS_PLAYER_PATH 
+                            , server -> introspection_data -> interfaces[0]
+                            , &interface_vtable
+                            , NULL  	/* user_data */
+                            , NULL  	/* user_data_free_func */
+                            , NULL); 	/* GError** */
+  	g_assert(server -> registration_id > 0);
+}
+
+static void on_name_acquired(GDBusConnection *connection,
+                  				const gchar *name,
+                  				gpointer user_data)
+{
+
+}
+
+static void on_name_lost (GDBusConnection *connection,
+              				const gchar *name,
+              				gpointer user_data)
+{
+
+}
+
+
+gint DB_mpris_server_start(DB_mpris_server **srv)
+{
+	g_type_init();
+
+	server = g_new(DB_mpris_server, 1);
+	if(server == NULL){
+		printf("Create DB_mpris_server error!!\n");
+		return DB_MPRIS_ERROR;
+	}
+	
+	server -> introspection_data = g_dbus_node_info_new_for_xml(introspection_xml
+																	, NULL);
+	if(server -> introspection_data == NULL){
+		printf("Create dbus node info error!!\n", __FILE__, __LINE__);
+		return DB_MPRIS_ERROR;
+	}
+
+	server -> owner_id = g_bus_own_name(G_BUS_TYPE_SESSION
+									, MPRIS_SERVICE
+									, G_BUS_NAME_OWNER_FLAGS_NONE
+									, on_bus_acquired
+									, on_name_acquired
+									, on_name_lost
+									, NULL, NULL);
+	
+	*srv = server;
+	return DB_MPRIS_OK;
+}
+gint DB_mpris_server_stop(DB_mpris_server *srv)
+{
+
+	g_free(srv);
+	return DB_MPRIS_OK;
+}
