6d7a1e56b7b06924926ccb2e541759f03a677ba2
huangcongyu2006 <huangcongyu2006@e141641c-d880-a6e4-911d-b54fb469a67a>
2011-06-17 06:14:12 +0000

    需要把tab扩展成空格。
    git-svn-id: https://deadbeef-mpris-plugin.googlecode.com/svn/trunk@9 e141641c-d880-a6e4-911d-b54fb469a67a

--- mpris_v1.c
+++ mpris_v1.c
@@ -43,12 +43,12 @@ enum{
  */
 void do_debug(const char *fmt, ...)
 {
-	va_list arg_ptr;
-	va_start(arg_ptr, fmt);
-	printf("\e[32m\e[1mMPRIS Debug Info: \e[0m\e[34m");
-	vprintf(fmt, arg_ptr);
-	printf("\e[0m\n");
-	va_end(arg_ptr);
+    va_list arg_ptr;
+    va_start(arg_ptr, fmt);
+    printf("\e[32m\e[1mMPRIS Debug Info: \e[0m\e[34m");
+    vprintf(fmt, arg_ptr);
+    printf("\e[0m\n");
+    va_end(arg_ptr);
 }
 
 struct _DB_mpris_server
