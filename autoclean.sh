f412ed4480e4bf7a9bb1e70341e97084efa47e9d
huangcongyu2006 <huangcongyu2006@e141641c-d880-a6e4-911d-b54fb469a67a>
2011-06-25 14:57:31 +0000

    修改autoclean.sh
    git-svn-id: https://deadbeef-mpris-plugin.googlecode.com/svn/trunk@28 e141641c-d880-a6e4-911d-b54fb469a67a

--- autoclean.sh
+++ autoclean.sh
@@ -2,17 +2,13 @@
 make distclean 
 rm -rf configure\
        Makefile.in\
-       depcomp\
-       config.guess\
-       config.sub\
-       ltmain.sh\
        config.h.in\
        autom4te.cache\
-       missing\
        aclocal.m4\
-       install-sh\
        m4/ltsugar.m4\
        m4/libtool.m4\
        m4/ltversion.m4\
        m4/lt~obsolete.m4\
-       m4/ltoptions.m4
+       m4/ltoptions.m4\
+       build-aux
+
