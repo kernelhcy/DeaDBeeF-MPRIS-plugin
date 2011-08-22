a8a6ace109fb43f25531c736105af607d94295d1
HuangCongyu <huangcongyu2006@gmail.com>
2011-08-22 18:11:34 +0800

    fix the copyright notice

--- mpris.c
+++ mpris.c
@@ -1,6 +1,6 @@
 /*
-    DeaDBeeF - ultimate music player for GNU/Linux systems with X11
-    Copyright (C) 2009-2011 Alexey Yakovenko <waker@users.sourceforge.net>
+    MPRIS plugin for Deadbeef player
+    Copyright (C) 2011 HuangCongyu <huangcongyu2006@gmail.com>
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
